2a0aafbef685a116f9f66501c1188169a0df9a83
mukulsoftwap <mukulsoftwap@gmail.com>
2017-02-18 16:50:50 +0530

    ng cli code

---
+++ server/api/models/Products.js
@@ -0,0 +1,21 @@
+/**
+ * Products.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name : "string",
+    discription : "string",
+    price : "number",
+    category : "string",
+    quantity : "number",
+    thum : "string",
+    discount : "number"
+  }
+  
+};
+
